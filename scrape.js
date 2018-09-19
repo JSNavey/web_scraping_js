ab3f5bde5a608b968c692d5854e8a915d4e7abaa
JSNavey <jsnavey@gmail.com>
2018-09-18 22:58:58 -0400

    Using cheerio to scrape the data and returns in json file

---
+++ scrape.js
@@ -0,0 +1,29 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+const URL =
+	'https://classifieds.ksl.com/search/?keyword=futon&zip=84101&miles=10&priceFrom=%240&priceTo=%24150&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=1DAY&city=&state=&sort=0';
+
+request(URL, (error, response, body) => {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  // console.log('body:', body);
+
+  const $ = cheerio.load(body);
+
+	// from view-source page, the data we need consist in script tag inside 'window.renderSearchSection'
+  const scripts = $('script').toArray();
+
+  scripts.forEach(script => {
+    if (script.children[0] !== undefined) {
+      if (script.children[0].data !== undefined)
+        if (script.children[0].data.includes('window.renderSearchSection')) {
+					let searchResults = script.children[0].data
+					// change the format to be json
+          const startIndex = searchResults.indexOf('(')
+          let results = eval(searchResults.substring(startIndex))
+          console.log(results)
+        }
+    }
+  });
+});
