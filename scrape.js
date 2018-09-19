7fbb2bac6297e49fbbfdb337fdb813bbcb8273f6
JSNavey <jsnavey@gmail.com>
2018-09-19 02:17:03 -0400

    Implement map function to get every titles out

--- scrape.js
+++ scrape.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const request = require('request');
 
 const URL =
-	'https://classifieds.ksl.com/search/?keyword=futon&zip=84101&miles=25&priceFrom=%240&priceTo=%24150&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=1DAY&city=&state=&sort=0';
+	'https://classifieds.ksl.com/search/?keyword=futon&zip=84101&miles=10&priceFrom=%240&priceTo=%24100&hasPhotos%5B%5D=Has+Photos&marketType%5B%5D=Sale&postedTimeFQ%5B%5D=7DAYS&city=&state=&sort=0';
 
 request(URL, (error, response, body) => {
   console.log('error:', error);
@@ -14,7 +14,7 @@ request(URL, (error, response, body) => {
 	// from view-source page, the data we need consist in script tag inside 'window.renderSearchSection'
   const scripts = $('script').toArray();
 
-  scripts.forEach(script => {
+  scripts.find(script => {
     if (script.children[0] !== undefined) {
       if (script.children[0].data !== undefined)
         if (script.children[0].data.includes('window.renderSearchSection')) {
@@ -22,10 +22,32 @@ request(URL, (error, response, body) => {
 					// change the format to be json
           const startIndex = searchResults.indexOf('(')
           let results = eval(searchResults.substring(startIndex))
-					console.log(results);
+					// console.log(results);
 					
-					let title = results.listings[0].title;
-					console.log(title);
+					// let itemName = results.listings[0].title;
+					// let price = results.listings[0].price;
+					// let location = results.listings[0].city;
+					// let addedAt = results.listings[0].createTime;
+					
+					// console.log('Item Name:', itemName);
+					// console.log('Price:', price);
+					// console.log('Location:', location);
+					// console.log('Added:', addedAt);
+					
+					let listItems = results.listings;
+					// console.log(listItems);
+					// for (let i = 0; i < listItems.length; i++) {
+					// 	console.log('Item Name:', listItems[i].title);
+					// 	console.log('Price:', listItems[i].price);
+					// 	console.log('Location:', listItems[i].city);
+					// 	console.log('Added:', listItems[i].createTime);
+					// }
+
+					let itemName = listItems.map(el => el.title);
+					console.log(itemName);
+
+					let price = listItems.map(el => el.price);
+					console.log(price);
         }
     }
   });
